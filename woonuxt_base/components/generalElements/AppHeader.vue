006f85779757d9bc2368711044e8ebbad4b4d053
Medani Hassene <medani.job@gmail.com>
2024-01-21 00:46:54 +0100

    pixel

--- woonuxt_base/components/generalElements/AppHeader.vue
+++ woonuxt_base/components/generalElements/AppHeader.vue
@@ -28,19 +28,3 @@ const { isShowingSearch } = useSearching();
   </header>
 </template>
 
-<script>
-export default {
-  name: 'App',
-  head: {
-    meta: [
-      // Other meta tags if needed
-    ],
-    script: [
-      {
-        defer: true,
-        src: 'https://msmgo.line.pm/pixel/3zPkNxNOzvolJuRV',
-      },
-    ],
-  },
-};
-</script>
