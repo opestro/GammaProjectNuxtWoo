caef4f29bbbd5b94d133942f0eddc37037671cf9
opestro <mahdiharzallah21@gmail.com>
2024-01-29 18:12:43 +0100

    pCards fix mobile responsiveness

--- woonuxt_base/components/generalElements/NewProducts.vue
+++ woonuxt_base/components/generalElements/NewProducts.vue
@@ -31,7 +31,7 @@
                                 </div>
 
                             </NuxtLink>
-                            <div class=" sm:flex-none md:flex justify-between items-center mx-2">
+                            <div class=" sm:flex-none md:flex md:justify-between items-center mx-2">
                                 <h1 v-if="pds.stock_status == 'instock'"
                                     class=" text-green-500 text-md items-center my-1  flex justify-center">
                                     <Icon name="ion:cube-outline" size="20" class="mr-2 " />{{ pds.stock_status || 0 }}
@@ -39,24 +39,24 @@
                                 <h1 v-else class=" text-md text-red-600 items-center my-1 flex justify-center">
                                     <Icon name="ion:cube-outline" size="20" class="mr-2" />{{ pds.stock_status || 0 }}
                                 </h1>
-                                <h1 class="text-base  flex justify-center  ">
-                                    <b>{{ pds.regular_price + ' DA' || 0 + ' DA' }}</b>
+                                <h1 class="text-base  flex justify-center  items-center ">
+                                    <b>{{ pds.regular_price + ' DA' || '0' + ' DA' }}</b>
                                 </h1>
 
                             </div>
                             <Button v-if="pds.stock_status == 'instock'"
-                                class="w-full flex justify-center  items-center h-12 sm:text-xs md:text-base truncate  border-1 bg-amber-600 text-white py-1 rounded-lg rounded-tl-none rounded-tr-none"
+                                class="w-full flex justify-center  items-center h-12 sm:text-xs md:text-base   border-1 bg-amber-600 text-white py-1 rounded-lg rounded-tl-none rounded-tr-none"
                                 @click="directBuy(pds.id, 0)">
-                                <Icon name="ion:cart-outline" size="20" class="mr-2" />{{
+                                <Icon name="ion:cart-outline" size="20" class="mr-2" /> <div class="text-xs md:text-base"> {{
                                     $t('messages.shop.buyDirect')
-                                }}
+                                }}</div>
                             </Button>
                             <Button v-else
-                                class="w-full flex justify-center  items-center h-12 sm:text-xs  md:text-base truncate border-1 bg-amber-600 text-white  py-1 rounded-lg rounded-tl-none rounded-tr-none disabled:cursor-not-allowed disabled:bg-gray-400 "
+                                class="w-full flex justify-center  items-center h-12 sm:text-xs  md:text-base  border-1 bg-amber-600 text-white  py-1 rounded-lg rounded-tl-none rounded-tr-none disabled:cursor-not-allowed disabled:bg-gray-400 "
                                 disabled @click="directBuy(pds.id, 0)">
-                                <Icon name="ion:cart-outline" size="20" class="mr-2" />{{
+                                <Icon name="ion:cart-outline" size="20" class="mr-2" /> <div class="text-xs md:text-base"> {{
                                     $t('messages.shop.buyDirect')
-                                }}
+                                }}</div>
                             </Button>
                         </card>
 
@@ -66,7 +66,7 @@
             <div class=" flex justify-center items-center container">
                 <div v-if="newProducts.isNew == false && newProducts.isLoading == true"
                     class="grid   max-sm:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 max-lg:grid-cols-5 overflow-x-auto gap-y-5 gap-x-3 lg:gap-x-5    p-2   ">
-                    <card class=" rounded-xl  flex-shrink-0  w-52  shadow-lg max-sm:w-50 sm:w-50 md:w-60 " v-for="i in i" :key="i">
+                    <card class=" w-40  md:w-60 rounded-xl  flex-shrink-0  shadow-lg  " v-for="i in i" :key="i">
                        
                             <cardTitle class="flex p-2 m-2 items-center">
                                 <div class="bg-gray-200 w-full animate-pulse h-52 rounded-2xl m-2"></div>
