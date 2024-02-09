3c1316908e635e41319dc915e3e3b8b7c51af119
opestro <mahdiharzallah21@gmail.com>
2024-02-09 21:19:09 +0100

    Delete PWA

--- woonuxt_base/nuxt.config.ts
+++ woonuxt_base/nuxt.config.ts
@@ -16,12 +16,7 @@ export default defineNuxtConfig({
 
   components: [{ path: resolve('./components'), pathPrefix: false }],
 
-  modules: ['woonuxt-settings', 'nuxt-graphql-client', '@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxt/image', '@nuxtjs/i18n' , '@pinia/nuxt', '@vite-pwa/nuxt'],
-  pwa : {
-    workbox : {
-  
-    }
-  },
+  modules: ['woonuxt-settings', 'nuxt-graphql-client', '@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxt/image', '@nuxtjs/i18n' , '@pinia/nuxt'],
   image: {
     domains: process.env.NUXT_IMAGE_DOMAINS ? process.env.NUXT_IMAGE_DOMAINS.replace(/ /g, '').split(',') : [],
   },
